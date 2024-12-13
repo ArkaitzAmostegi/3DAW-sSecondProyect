960be6d0449cf7250d4ad049320a41594b835f75
Arkaitz Amostegi <ikdge@plaiaundi.net>
2024-12-13 17:37:16 +0100

    Inicializando SecondProyect

---
+++ libro.java
@@ -0,0 +1,15 @@
+public class libro {
+    private String titulo=" ";// Título del libro.
+    private String autor; //Autor del libro.
+    private int añoPublicacion; //Año de publicación.
+    private String genero; //Género del libro.
+
+    public libro() {
+        titulo= "Desconocido";
+        autor="Anonimo";
+        añoPublicacion= 2000;
+        genero="general";
+    }
+
+
+}
---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+// Ejercicio 1: Clase Libro
+    // Requerimientos:
+        // Implementa una clase Libro con los siguientes atributos:
+        // titulo (String): Título del libro.
+        // autor (String): Autor del libro.
+        // añoPublicacion (int): Año de publicación.
+        // genero (String): Género del libro.
+        // Crea los siguientes constructores:
+        // Por defecto, con valores genéricos ("Desconocido", "Anónimo", 2000, "General").
+        // Con parámetros para inicializar todos los atributos.
+        // Parcial, que inicialice solo el título y el autor, dejando los demás con valores por defecto.
+        // Implementa un método toString() para mostrar los datos del libro de forma legible.
+        // Crea una clase principal MainLibro para probar los tres constructores.
+    }
+}

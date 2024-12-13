880f6f4448388029df1bbe6166fea89c887e8330
Jokin Berridi <ikdgg@plaiaundi.net>
2024-12-13 22:06:29 +0100

    primerCommitJokin

--- libro.java
+++ libro.java
@@ -1,15 +1,30 @@
-public class libro {
+public class Libro {
     private String titulo=" ";// Título del libro.
     private String autor; //Autor del libro.
     private int añoPublicacion; //Año de publicación.
     private String genero; //Género del libro.
 
-    public libro() {
-        titulo= "Desconocido";
-        autor="Anonimo";
-        añoPublicacion= 2000;
-        genero="general";
+    //Constructor vacio
+    public Libro() {
+        this.titulo= "Desconocido";
+        this.autor="Anonimo";
+        this.añoPublicacion= 2000;
+        this.genero="General";
     }
-
+    //Constructor con todos los atributos
+    public Libro(String titulo, String autor, int añoPublicacion, String genero) {
+		super();
+		this.titulo=titulo;
+        this.autor=autor;
+        this.añoPublicacion=añoPublicacion;
+        this.genero=genero;
+	}
+    //Constructor con algunos atributos
+    public Libro(String titulo, String autor){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.añoPublicacion=2000;
+        this.genero="General";
+    }
 
 }
--- src/App.java
+++ src/App.java
@@ -1,25 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-// Ejercicio 1: Clase Libro
-    // Implementa una clase Libro con los siguientes atributos:
-
-    // titulo (String): Título del libro.
-    // autor (String): Autor del libro.
-    // añoPublicacion (int): Año de publicación.
-    // genero (String): Género del libro.
-    // Requerimientos:
-
-    // Crea un constructor por defecto que inicialice los atributos con valores genéricos ("Desconocido", "Anónimo", 2000, "General").
-    // Crea un constructor con parámetros que permita inicializar todos los atributos con valores dados.
-    // Crea un constructor parcial que inicialice solo el título y el autor, dejando los demás con valores por defecto.
-    // Implementa un método toString() para mostrar los datos del libro de forma legible.
-    // Clase principal MainLibro:
-
-    // Crea 3 objetos de la clase Libro:
-    // Uno usando el constructor por defecto.
-    // Uno usando el constructor con parámetros para inicializar todos los atributos.
-    // Uno usando el constructor parcial.
-    // Muestra la información de cada libro utilizando el método toString().
 
     }
 }

de3aefbfef4a3cdb9f6cfa469461a1865ce2467d
Arkaitz Amostegui <ikdge@plaiaundi.net>
2024-12-13 19:24:48 +0100

    Modificaciones en el enunciado

--- src/App.java
+++ src/App.java
@@ -1,17 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
 // Ejercicio 1: Clase Libro
+    // Implementa una clase Libro con los siguientes atributos:
+
+    // titulo (String): Título del libro.
+    // autor (String): Autor del libro.
+    // añoPublicacion (int): Año de publicación.
+    // genero (String): Género del libro.
     // Requerimientos:
-        // Implementa una clase Libro con los siguientes atributos:
-        // titulo (String): Título del libro.
-        // autor (String): Autor del libro.
-        // añoPublicacion (int): Año de publicación.
-        // genero (String): Género del libro.
-        // Crea los siguientes constructores:
-        // Por defecto, con valores genéricos ("Desconocido", "Anónimo", 2000, "General").
-        // Con parámetros para inicializar todos los atributos.
-        // Parcial, que inicialice solo el título y el autor, dejando los demás con valores por defecto.
-        // Implementa un método toString() para mostrar los datos del libro de forma legible.
-        // Crea una clase principal MainLibro para probar los tres constructores.
+
+    // Crea un constructor por defecto que inicialice los atributos con valores genéricos ("Desconocido", "Anónimo", 2000, "General").
+    // Crea un constructor con parámetros que permita inicializar todos los atributos con valores dados.
+    // Crea un constructor parcial que inicialice solo el título y el autor, dejando los demás con valores por defecto.
+    // Implementa un método toString() para mostrar los datos del libro de forma legible.
+    // Clase principal MainLibro:
+
+    // Crea 3 objetos de la clase Libro:
+    // Uno usando el constructor por defecto.
+    // Uno usando el constructor con parámetros para inicializar todos los atributos.
+    // Uno usando el constructor parcial.
+    // Muestra la información de cada libro utilizando el método toString().
+
     }
 }

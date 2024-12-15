7a06d0c2695203596d225ac83ab57ca5b82abf9d
Arkaitz Amostegui <ikdge@plaiaundi.net>
2024-12-15 13:06:37 +0100

    Errores solucionados y creados los tres objetos

--- libro.java
+++ libro.java
@@ -1,18 +1,19 @@
-public class Libro {
+public class libro {
+
     private String titulo=" ";// Título del libro.
     private String autor; //Autor del libro.
     private int añoPublicacion; //Año de publicación.
     private String genero; //Género del libro.
 
     //Constructor vacio
-    public Libro() {
+    public libro() {
         this.titulo= "Desconocido";
         this.autor="Anonimo";
         this.añoPublicacion= 2000;
         this.genero="General";
     }
     //Constructor con todos los atributos
-    public Libro(String titulo, String autor, int añoPublicacion, String genero) {
+    public libro(String titulo, String autor, int añoPublicacion, String genero) {
 		super();
 		this.titulo=titulo;
         this.autor=autor;
@@ -20,7 +21,7 @@ public class Libro {
         this.genero=genero;
 	}
     //Constructor con algunos atributos
-    public Libro(String titulo, String autor){
+    public libro(String titulo, String autor){
         this.titulo=titulo;
         this.autor=autor;
         this.añoPublicacion=2000;
--- src/Libro.java
+++ src/Libro.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Libro {
     public static void main(String[] args) throws Exception {
 
+        Scanner sc= new Scanner(System.in);
+
+        Libro libro1=new Libro();
+        Libro libro2=new Libro();
+        Libro libro3=new Libro();
+
+
+        sc.close();
     }
 }
